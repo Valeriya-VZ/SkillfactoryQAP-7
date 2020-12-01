b0bd32a16f415fa92bb4169077cd4ac37ee77186
Valeryia Zarozhnaya <valeriya.zarozhnaya@gmail.com>
2020-12-01 17:47:53 +0300

    update

---
+++ Модуль 16/16.10.5.py
@@ -0,0 +1,12 @@
+# Создать класс SquareException.
+# Добавить в конструктор класса Square собственное исключение NonPositiveDigitException,
+# унаследованное от ValueException, которое будет срабатывать каждый раз,
+# когда сторона квадрата меньше или равна 0.
+class NonPositiveDigitException(ValueError):
+    pass
+
+
+class Square:
+    def __init__(self, a):
+        if a < 0:
+            raise NonPositiveDigitException('Неправильно указанна сторона квадрата')
